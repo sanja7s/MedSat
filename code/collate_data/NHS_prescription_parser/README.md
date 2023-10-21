7028d620554bb3d7ee6cac4e67d69dee849bb619
Scepanovic Sanja <sanja.scepanovic@nokia-bell-labs.com>
2023-10-21 12:43:10 +0100

    added the final bit into NHS prescription allowing to extract a condition prevalence given a custom drug list. updated readme accordingly.

--- code/collate_data/NHS_prescription_parser/README.md
+++ code/collate_data/NHS_prescription_parser/README.md
@@ -1,21 +1,21 @@
 
-# Submodule to compute drug prevalences. 
+# Submodule to compute drug or condition prevalences. 
 This module contains code to compute LSOA level prevalences given 
-1. A list of drug names 
-2. A list of drugnames with their respective BNF codes. 
-3. A list of conditions/symptoms, provided we can find associated drugs from drugbank 
-4. Opioid OME prevalence 
+1. A (list of) drug name(s). 
+2. A list of drug names with their respective BNF codes. 
+3. A list of conditions/symptoms, provided we can find associated drugs from drugbank/GPT/we curated it. 
+4. Opioid OME prevalence.
 
 ## Installing. 
 Simply create a conda environment ```conda env create -f environment.yml```
 Activate the environment 
 
-## Prevalence based on drug names 
+## 1 Prevalence based on on (a list of) drug name(s). 
 This will generate prevalence values for a list of drug names. The script will try to find BNF codes for variants of the said drug names and then compute LSOA level prevalences. 
 - Cd into the code directory
 - run the script as such ```python drug_prevalence.py -d metformin -s <Start_YYYYMM> -e <End_YYYYMM>```
 
-## Prevalence based on list of Drug names 
+## 2 Prevalence based on list of drug names with BNF codes.
 This is a toned down version of the above script which takes in a list of drugs and their corresponding BNF codes as a JSON and computes prevalences. Sample list jsons are included. 
 In order to convert an arbitrary list of drugs into this format, you may use the following prompt with GPT completion API. Make sure you douple check the response and the corresponding BNF codes. Also make sure that the temperature is set to 0 and maximum length to its maximum limit. 
 
@@ -29,19 +29,33 @@ You are a pharmacy expert who knows the British National Formulary (BNF) codes o
   "codine": ["0309020AB"]
 }
 ```
-
 once you have the json ready, you may run the prevalence script as follows
 
-```
-python custom_list_prevalence.py -l ./sample_list_antidepressants.json -s 201901 -e 201901
+```python custom_list_prevalence.py -l ./sample_list_antidepressants.json -s 201901 -e 201901```
 
-```
-## Prevalence for conditions/symptoms
+## 3 Prevalence for conditions/symptoms.
 There are two ways to run this use case. 
 
-### Using GPT as a aid to find drugs. 
-In this case, the first step is to run the follwing prompt before asking GPT model about a particular condition. Please note that
-the prompt to be run on GPT 3.5 Turbo, max token limit set to maximum, and temperature set to 0.
+### a) Using drugbank as an aid to find drugs
+
+This part mathes names of conditions with a drug->condition association graph built from the drugbank crawl. 
+
+To generate prevalences using condition names 
+
+```python condition_prevalence.py -s YYYYMM -e YYYYMM -c condition_name1 condition_name2```
+
+The prevalences files should be created in data_prep folder, along with a csv file called ```Drugs.csv```that contains associations of named conditions with different drugs and BNF codes.
+
+
+### b) Using custom condition drug name lists.
+
+Here, we have an option b.1) to use a literature-derived or user-curated list of drugs (e.g., `sample_list_antidepressants.json`), or b.2) we can use GPT as an alternative to drugbank to help us associate drugs with the condition. 
+
+#### b.1) Using drug names list we curated from literature or ourselves.
+Example of lists curated by us or from the literature are `sample_list_antidepressants.json`, `sample_list_anxiety.json` or `sample_list_painkiller.json`.
+
+#### b.2) Using GPT as an aid to find drugs.
+In this case, the first step is to run the follwing prompt before asking GPT model about a particular condition. Please note that the prompt is to be run on GPT 3.5 Turbo, max token limit set to maximum, and temperature set to 0.
 
 ```
 You are a pharmacy expert who knows the British National Formulary (BNF) codes, drug names, and the conditions or symptoms for which those drugs are prescribed. You follow the following guidelines 
@@ -62,12 +76,7 @@ The user will send a name of a condition or a symptom, and you must respond in a
 }
 ``` 
 The response should be a wellformed JSON with the mapped drugs for the given condition. 
+Transform this list into the format such as in `sample_list_antidepressants.json`. Having such a list of drug names associated with our condition, we can then run the following commands:
 
-### Using general condition names. 
-
-This part mathes names of conditions with a drug->condition association graph built from the drugbank crawl. 
-To generate prevalences using condition names 
-
-```python condition_prevalence.py -s YYYYMM -e YYYYMM -c condition_name1 condition_name2```
-
-The prevalences files should be created in data_prep folder, along with a csv file called ```Drugs.csv```that contains associations of named conditions with different drugs and BNF codes.
+```python custom_list_prevalence.py -l ./sample_list_antidepressants.json -s 201901 -e 202012```
+``` python condition_prevalence.py -s 201901 -e 202012 -c depression -custDLFN sample_list_antidepressants.json```
--- code/collate_data/NHS_prescription_parser/code/condition_prevalence.py
+++ code/collate_data/NHS_prescription_parser/code/condition_prevalence.py
@@ -8,12 +8,14 @@ from matching.commonFunc import writeResultFiles, calculateTemporalMetrics_LSOA
 from matching.drugMatching import DrugMatcher
 from sources.downloader import Downloader
 from tqdm import tqdm
-
+import os
+import json
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c' , '--conditions' ,nargs="+" ,help="list of a conditions you need prescription prevalence for. Can be a list of single item ['item'] ")
+    parser.add_argument('-c' , '--conditions' ,nargs="+" ,help="list of a conditions you need prescription prevalence for. Can be a list or a single item ['item'] ")
+    parser.add_argument('-custDLFN' , '--custom_drug_list_file_names' ,nargs="+", default=None, help="list of custom lists for the conditions you need prescription prevalence for. Can be a list of lists or a single list ['list'] ")
     parser.add_argument('--isCat' , type=str2bool , default=False, help=" Boolean flag to notify if the list is a set of conditions or categories. Default is False, implying conditions ")
     parser.add_argument('-s', "--start" , help="start year and month, format YYYYMM")
     parser.add_argument('-e', "--end" , help="end year and month, format YYYYMM")
@@ -24,82 +26,136 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     conditions = args.conditions
-    print("Running for : ",conditions)
+    print("Running for : ", conditions)
     
     start = str(args.start)
     end = str(args.end)
+    custDLFN = args.custom_drug_list_file_names
 
     odir = args.output_dir
-
 
     if odir:
         print('overriding output dir')
         output_dir = odir
 
-    downloader = Downloader(sourcesFile="sources/serialized_file_paths.json" , download_dir=input_dir)
-    selected_files = []
-    try:
-        selected_files = downloader.download_range(start , end)
-    except:
-        print("Failed download")
-        exit(-1)
-    files_sub = selected_files
+    if custDLFN is None:
 
-    print (f'Running for suplied drugs between months {start} and {end}. The following files were selected \n\n\n')
-    for f in files_sub:
-        print(f)
+        downloader = Downloader(sourcesFile="sources/serialized_file_paths.json" , download_dir=input_dir)
+        selected_files = []
+        try:
+            selected_files = downloader.download_range(start , end)
+        except:
+            print("Failed download")
+            exit(-1)
+        files_sub = selected_files
 
-    print (f'Running for {conditions} between months {start} and {end}. The following files were selected \n\n\n')
-    for f in files_sub:
-        print(f)
+        print (f'Running for suplied drugs between months {start} and {end}. The following files were selected \n\n\n')
+        for f in files_sub:
+            print(f)
+
+        print (f'Running for {conditions} between months {start} and {end}. The following files were selected \n\n\n')
+        for f in files_sub:
+            print(f)
 
     
-    matcher = DrugMatcher(mappings_dir = './mappings/' , output_dir=output_dir)
+        matcher = DrugMatcher(mappings_dir = './mappings/' , output_dir=output_dir)
 
-    DiseaseDrugs, drugMap = matcher.DrugMatching(conditions)
+        DiseaseDrugs, drugMap = matcher.DrugMatching(conditions)
 
-    matcher.dumpDrugs(DiseaseDrugs)
-    
-    print(DiseaseDrugs)
-    
-    for k in drugMap:
-        print(k , drugMap[k])
-
-    monthly_borough_dosage_new = {}
-    monthly_borough_costs_new = {}
-    monthly_borough_quantity_new = {}
-    monthly_borough_items_new = {}
-
-
-    #Compute prevalence over selected files
-
-    for f in tqdm(files_sub):
-        month = f.split('/')[-1].split('.')[0]
-        print("Working with file  " + f)
-        old = False
+        matcher.dumpDrugs(DiseaseDrugs)
         
-        monthly_borough_dosage_new[month] = {}
-        monthly_borough_costs_new[month] = {}
-        monthly_borough_quantity_new[month] = {}
-        monthly_borough_items_new[month] = {}
+        print(DiseaseDrugs)
+        
+        for k in drugMap:
+            print(k , drugMap[k])
 
-        pdp = pd.read_csv(f,compression='gzip')
-        for disease in tqdm(drugMap):
-            print("Working with disease  " + disease)
-            monthly_borough_dosage_new[month][disease] = {}
-            monthly_borough_costs_new[month][disease] = {}
-            monthly_borough_quantity_new[month][disease] = {}
-            monthly_borough_items_new[month][disease] = {}
+        monthly_borough_dosage_new = {}
+        monthly_borough_costs_new = {}
+        monthly_borough_quantity_new = {}
+        monthly_borough_items_new = {}
 
 
-            drugs = drugMap[disease]
-            opioids = pdp.loc[pdp['16'].isin(drugs)] #Original opioids
+        #Compute prevalence over selected files
 
-            monthly_borough_quantity_new[month][disease] , monthly_borough_costs_new[month][disease], monthly_borough_dosage_new[month][disease]  , monthly_borough_items_new[month][disease] = calculateTemporalMetrics_LSOA(opioids, mappings_dir='./mappings/', old = old)
+        for f in tqdm(files_sub):
+            month = f.split('/')[-1].split('.')[0]
+            print("Working with file  " + f)
+            old = False
+            
+            monthly_borough_dosage_new[month] = {}
+            monthly_borough_costs_new[month] = {}
+            monthly_borough_quantity_new[month] = {}
+            monthly_borough_items_new[month] = {}
+
+            pdp = pd.read_csv(f,compression='gzip')
+            for disease in tqdm(drugMap):
+                print("Working with disease  " + disease)
+                monthly_borough_dosage_new[month][disease] = {}
+                monthly_borough_costs_new[month][disease] = {}
+                monthly_borough_quantity_new[month][disease] = {}
+                monthly_borough_items_new[month][disease] = {}
 
 
-    print("Done computing LSOA level prescription prevalences, writing files")
+                drugs = drugMap[disease]
+                opioids = pdp.loc[pdp['16'].isin(drugs)] #Original opioids
+
+                monthly_borough_quantity_new[month][disease] , monthly_borough_costs_new[month][disease], monthly_borough_dosage_new[month][disease], monthly_borough_items_new[month][disease] = calculateTemporalMetrics_LSOA(opioids, mappings_dir='./mappings/', old = old)
+
+                print("Done computing LSOA level prescription prevalences, writing files")
+
+                writeResultFiles(monthly_borough_quantity_new ,monthly_borough_dosage_new , monthly_borough_costs_new , monthly_borough_items_new , conditions , output_dir,  mappings_dir='./mappings/' , isOld = old)
+                print("Finished processing !!!!! ")
+    else:
+        
+        directory_path = output_dir
+
+        i = 0
+        for cond in conditions:
+            print (f"Parsing condition {i+1} of {len(conditions)}: {cond}")
+            all_files = []
+
+            with open(custDLFN[i], 'r') as drug_file:
+                custom_drug_list = json.load(drug_file)
+
+            for drug_name in custom_drug_list:
+
+                file_name = f"{drug_name}_V4.csv.gz"
+                all_files.append(os.path.join(directory_path, file_name))
+
+            # List to hold dataframes from each file
+            dfs = []
+
+            # Read all CSV files in the directory into dataframes and append to the list
+            for file_name in all_files:
+                dfs.append(pd.read_csv(file_name,compression='gzip'))
+
+            # Concatenate all the dataframes
+            df = pd.concat(dfs, ignore_index=True)
+
+            # Check that 'Patient_count' is the same across the same 'LSOA_CODE's
+            assert df.groupby('LSOA_CODE')['Patient_count'].nunique().max() == 1
+
+            # Aggregate the required columns
+            agg_funcs = {
+                'Total_quantity': 'sum',
+                'Dosage_ratio': 'sum',
+                'Total_cost': 'sum',
+                'Total_items': 'sum',
+                'Patient_count': 'first'
+            }
+            disease_df = df.groupby(['YYYYMM', 'LSOA_CODE']).agg(agg_funcs).reset_index()
+
+            # Save to a new CSV file
+            out_file_name = f"{cond}_V4.csv.gz"
+            
+            print("Done computing LSOA level prescription prevalences, writing files")
+            disease_df.to_csv(os.path.join(directory_path, out_file_name),index=False,compression='gzip')
+            print("Finished processing !!!!! ")
+            
+
+            i+=1
 
 
-    writeResultFiles(monthly_borough_quantity_new ,monthly_borough_dosage_new , monthly_borough_costs_new , monthly_borough_items_new , conditions , output_dir,  mappings_dir='./mappings/' , isOld = old)
-    print("Finished processing !!!!! ")
+    
+    
+
